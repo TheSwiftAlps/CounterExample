b9b0ccda6838ad0f70cd7344700672f05f7b7529
Elvis Nuñez <elvisnunez@me.com>
2016-11-11 15:28:34 +0100

    Add diffing animation

--- CounterExample/State/AppState.swift
+++ CounterExample/State/AppState.swift
@@ -1,6 +1,17 @@
 import ReSwift
 
-typealias Counter = Int
+struct Counter: Equatable {
+    let index: Int
+    var value: Int = 0
+
+    init(index: Int) {
+        self.index = index
+    }
+
+    static func ==(lhs: Counter, rhs: Counter) -> Bool {
+        return lhs.index == rhs.index
+    }
+}
 
 struct AppState: StateType {
     var counters: [Counter] = []
